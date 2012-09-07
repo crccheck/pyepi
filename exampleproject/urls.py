b16638a7821eae64df7cf9081384d23d099193e6
crccheck <c@crccheck.com>
2012-09-07 11:45:32 -0500

    implement media url
    the whole only serve with DEBUG is a lie, but it works, cargo cult FTW

--- exampleproject/urls.py
+++ exampleproject/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -16,3 +17,11 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
